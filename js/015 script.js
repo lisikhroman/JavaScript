8d73cb63704efff0cbc797acda74ded780cfecc4
DESKTOP-CUGMPBP\79234 <Roman.Lisikh@indusoft.ru>
2023-01-25 16:48:09 +0700

    implements_HW2

--- js/015 script.js
+++ js/015 script.js
@@ -14,4 +14,23 @@
 
 'use strict';
 
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
+    personalMovieDB.movies[a] = b;
+}
+
+console.log(personalMovieDB);
+
+
 // Код возьмите из предыдущего домашнего задания
