b3df04ce9e89dfe3ccf73a08e935d900b40892ec
DESKTOP-CUGMPBP\79234 <Roman.Lisikh@indusoft.ru>
2023-01-31 14:54:06 +0700

    lesson 47, 48

--- 038 Food-dist/js/script.js
+++ 038 Food-dist/js/script.js
@@ -255,4 +255,105 @@ window.addEventListener('DOMContentLoaded', () => {
     //2) Контекст у методов объекта - сам объект
     //3) this в конструкторах и классах - это новый экземпляр объекта
     //4) Ручная привязка this: call, apply, bind.
+
+
+
+    //Class
+
+    class Rectangle {
+        constructor(height, weight) {
+            this.height = height;
+            this.weight = weight;
+        }
+
+        calcArea() {
+            return this.height * this.weight;
+        }
+    }
+
+    class ColoredRectangleWithText extends Rectangle {
+        constructor(height, width, text, color) {
+            super(height, width);
+            this.text = text;
+            this.color = color;
+        }
+
+        showMyProps() {
+            console.log(`Текст: ${this.text}, цвет: ${this.color}`)
+        }
+    }
+
+    const div = new ColoredRectangleWithText(25, 10, 'hello', 'red')
+    div.showMyProps();
+    console.log(div.calcArea())
+
+    // const square = new Rectangle(2, 4);
+    // console.log(square.calcArea())
+
+    //Используем классы
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. '
+    + 'Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    8,
+'.menu .container'
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд.'
+    + 'Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        9,
+        '.menu .container'
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения,'
+    + 'молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        10,
+        '.menu .container'
+    ).render();
+
 });
