992a453f7c00494ed83c48382b639d58deee6452
DESKTOP-CUGMPBP\79234 <Roman.Lisikh@indusoft.ru>
2023-02-01 10:14:57 +0700

    lesson 52

---
+++ calc/js/script.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const inputRub = document.querySelector('#rub'),
+    inputUsd = document.querySelector('#usd');
+
+inputRub.addEventListener('change', () => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('load', () => {
+        if (request.readyState === 4 && request.status === 200) {
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+        } else {
+            alert(request.status + request.statusText);
+        }
+    })
+
+    // status
+    // statusText
+    // response
+    // readyState
+
+
+})
